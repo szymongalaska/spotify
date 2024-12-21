d4e0e69fd22373c4619546cd085c5e1badddbb3e
Szymon Gałąska <siubidubasiabadabba@gmail.com>
2024-12-21 13:06:49 +0100

    Added createPlaylist method to SpotifyApiComponent

--- src/Controller/Component/SpotifyApiComponent.php
+++ src/Controller/Component/SpotifyApiComponent.php
@@ -864,4 +864,28 @@ class SpotifyApiComponent extends Component
 
         return $this->_batchRetrieveData($url);
     }
+
+    /**
+     * Create a playlist for a Spotify user. (The playlist will be empty until you add tracks.) Each user is generally limited to a maximum of 11000 playlists.
+     * 
+     * @param string $playlistName The name for the new playlist, for example "Your Coolest Playlist". This name does not need to be unique; a user may have several playlists with the same name.
+     * @param string $playlistDescription value for playlist description as displayed in Spotify Clients and in the Web API.
+     * @param bool $public Defaults to true. The playlist's public/private status (if it should be added to the user's profile or not): true the playlist will be public, false the playlist will be private. To be able to create private playlists, the user must have granted the playlist-modify-private scope. For more about public/private status, see Working with Playlists
+     * @param bool $collaborative Defaults to false. If true the playlist will be collaborative. Note: to create a collaborative playlist you must also set public to false. To create collaborative playlists you must have granted playlist-modify-private and playlist-modify-public scopes.
+     * 
+     * @return array|null
+     */
+    public function createPlaylist(string $playlistName, string $playlistDescription, bool $public = true, bool $collaborative = false)
+    {
+        $userId = $this->getProfile()['id'];
+
+        $data = [
+            'name' => $playlistName,
+            'description' => $playlistDescription,
+            'public' => $public,
+            'collaborative' => $collaborative
+        ];
+
+        return $this->request('POST', self::API_URL.'/v1/users/'.$userId.'/playlists', json_encode($data));
+    }
 }
--- tests/TestCase/Controller/Component/SpotifyApiComponentTest.php
+++ tests/TestCase/Controller/Component/SpotifyApiComponentTest.php
@@ -809,4 +809,25 @@ class SpotifyApiComponentTest extends TestCase
 
         $this->SpotifyApi->getRecentlyPlayedTracks(20, 1734739200, 1734739200);
     }
+
+    public function testCreatePlaylist()
+    {
+        $this->createSpotifyMock(['request', 'getProfile']);
+
+        $this->SpotifyApi->expects($this->once())->method('getProfile')->willReturn(['id' => 'testUser']);
+
+        $inputData = ['name' => 'testName', 'description' => 'testDescription', 'public' => false, 'collaborative' => false];
+        $createPlaylistFixture = $this->getJsonFixture('create-playlist');
+        $this->SpotifyApi->expects($this->once())->method('request')->with('POST', 'https://api.spotify.com/v1/users/testUser/playlists', json_encode($inputData))->willReturn($createPlaylistFixture);
+
+        $result = $this->SpotifyApi->createPlaylist('testName', 'testDescription', false, false);
+
+        $this->assertIsArray($result);
+        $this->assertArrayHasKey('id', $result);
+        $this->assertEquals('testDescription', $result['description'], );
+        $this->assertEquals('testName', $result['name']);
+        $this->assertFalse($result['public']);
+        $this->assertFalse($result['collaborative']);
+        $this->assertEquals('testUser', $result['owner']['id']);
+    }
 }
